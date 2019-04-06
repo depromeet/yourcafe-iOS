5b7df7b36faa3b83bb98b81981098ba4dcbe1958
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-06 20:53:17 +0900

    Add PullUpControlView on MapViewController

--- YourCafe/ViewControllers/Main/MapViewController.swift
+++ YourCafe/ViewControllers/Main/MapViewController.swift
@@ -12,16 +12,58 @@ import NMapsMap
 class MapViewController: UIViewController {
     @IBOutlet weak var naverMapView: NMFNaverMapView!
     
+    private var pullUpControlView: PullUpControlView? = PullUpControlView.instantiateFromNib()
+    private var pullUpControlViewHeightConstraint: NSLayoutConstraint?
+    
+    private var pullUpControlViewHeight: CGFloat = 70
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupMapView()
+        setPullUpControlView()
     }
     
-    func setupMapView() {
+    private func setupMapView() {
         naverMapView.mapView.zoomLevel = 15
         naverMapView.showLocationButton = true
         naverMapView.showZoomControls = false
         naverMapView.showCompass = false
+    }
+    
+    private func setPullUpControlView() {
+        guard let controlView = pullUpControlView else { return }
+        controlView.delegate = self
+        controlView.dataSource = self
+        
+        layoutPullUpControlView()
+    }
+    
+    private func layoutPullUpControlView() {
+        guard let controlView = pullUpControlView else { return }
+        controlView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(controlView)
+        
+        controlView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        controlView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        controlView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        pullUpControlViewHeightConstraint = controlView.heightAnchor.constraint(equalToConstant: pullUpControlViewHeight)
+        pullUpControlViewHeightConstraint?.isActive = true
+    }
+}
+
+extension MapViewController: PullUpControlViewDataSource {
+    func heightOfContainerView(_ pullUpSearchControlView: PullUpControlView) -> CGFloat {
+        return view.frame.height
+    }
+    
+    func heightOfSearchControlView(_ pullUpSearchControlView: PullUpControlView) -> CGFloat {
+        return pullUpControlViewHeight
+    }
+}
+
+extension MapViewController: PullUpControlViewDelegate {
+    func pullUpControlView(_ pullUpControlView: PullUpControlView, didPanned height: CGFloat) {
+        pullUpControlViewHeightConstraint?.constant = height
     }
 }
